3d402ad55cfb6624ebe5703af1c41f39d43c0800
Vasyl.Burlaka <vasiliy.burlaka@gmail.com>
2021-01-02 23:00:22 +0200

    init

---
+++ BlockBreaker/Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Uhu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
